5c5bb0f553d3179044e7118278d62edac0d2365d
Trifon <trifon221@gmail.com>
2023-04-06 15:10:53 +0300

    Revert "Merge branch 'scanning' into main"
    This reverts commit 7fbd74249d2f1293f93c4fe6d63c873f73d9657f.

--- Assets/Scripts/Grid System/Node.cs
+++ Assets/Scripts/Grid System/Node.cs
@@ -1,12 +1,13 @@
-using JetBrains.Annotations;
-using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Node : MonoBehaviour
+public class Node : NetworkBehaviour
 {
     public Vector3 position;
+    public bool isPlayerTurn = true;
+    public bool damaged;
     public bool isDestroyed = false;
     public bool isScanned = false;
     public bool isWalkable = true;
@@ -15,9 +16,12 @@ public class Node : MonoBehaviour
     public int row;
     public int column;
     public List<Node> neighbours = new List<Node>();
+
+    public Mesh destroyedNodeMesh;
+    public Material destroyedNodeMaterial;
+
     public UnityEvent OnClick;
 
-    Color[] nodeColors;  
     public List<Node> GetNeighbours()
     {
         return neighbours;
@@ -30,6 +34,26 @@ public class Node : MonoBehaviour
             neighbours.Add(neighbour);
         }
     }
+
+    //public void DestroyNode(Node node)
+    //{
+    //    node.isDestroyed = true;
+    //    node.isWalkable = false;
+    //    if (node.occupyingObject != null)
+    //    {
+    //        //node.occupyingObject.GetComponent<TankScript>().TakeHealth(node);
+    //        node.occupyingObject = null;
+    //        return;
+    //    }
+    //    MeshFilter meshFilter = node.GetComponent<MeshFilter>();
+    //    MeshRenderer meshRendered = node.GetComponent<MeshRenderer>();
+    //    Vector3 nodePosition = node.transform.position + new Vector3(5f, -1f, 0.0f);
+    //    Mesh destroyedMesh = Instantiate(destroyedNodeMesh);
+
+    //    meshFilter.mesh = destroyedMesh;
+    //    meshRendered.material = destroyedNodeMaterial;
+    //}
+
     public void DestroyNode(Node node)
     {
         node.isDestroyed = true;
@@ -45,35 +69,6 @@ public class Node : MonoBehaviour
         GameObject destroyedNode = Instantiate(destroyedObjectPrefab, node.transform.position, node.transform.rotation);
         destroyedNode.transform.SetParent(node.transform.parent);
         node.GetComponent<NetworkObject>().Despawn();
-    }
-
-    public void ScanNode()
-    {
-        this.isScanned = true;      
-        nodeColors = new Color[this.gameObject.GetComponent<Renderer>().materials.Length]; // initialize the nodeColors array with the same length as the array of materials
-
-        for (int i = 0; i < this.gameObject.GetComponent<Renderer>().materials.Length; i++)
-        {
-            nodeColors[i] = this.gameObject.GetComponent<Renderer>().materials[i].color;
-            this.gameObject.GetComponent<Renderer>().materials[i].color = Color.red;
-           
-        }
-        StartCoroutine(ResetColorCoroutine(this));
-        if (this.occupyingObject != null && this.occupyingObject.CompareTag("tank1"))
-        {
-            this.gameObject.GetComponent<Renderer>().materials[1].color = Color.blue;
-            Debug.Log("Player found on row " + this.row + " column: " + this.column);
-        }
-
-    }
-    private IEnumerator ResetColorCoroutine(Node node)
-    {
-        yield return new WaitForSeconds(1.5f);
-
-        for (int i = 0; i < nodeColors.Length; i++)
-        {
-            node.gameObject.GetComponent<Renderer>().materials[i].color = nodeColors[i];
-        }
     }
 
     private void OnMouseDown()
