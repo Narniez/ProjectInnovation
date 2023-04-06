f1b4cbeff899c14f98b4a5e3c08d749c70d4904d
Stanislav <taix97@outlook.com>
2023-04-06 14:13:10 +0300

    Revert "Merge branch 'multitashak' into scanning"
    This reverts commit cb854ee8932cc2355b0be64ce249f13fae65f144.

--- Assets/Scripts/Grid System/Grid.cs
+++ Assets/Scripts/Grid System/Grid.cs
@@ -1,8 +1,7 @@
 using System.Collections.Generic;
-using Unity.Netcode;
 using UnityEngine;
 
-public class Grid : NetworkBehaviour
+public class Grid : MonoBehaviour
 {
     public static Dictionary<Node, Vector3Int> nodesDictionary = new Dictionary<Node, Vector3Int>();
 
@@ -22,11 +21,8 @@ public class Grid : NetworkBehaviour
     public List<GameObject> nodesPrefab;
     public GameObject player;
     public bool playerShoot = false;
-
-    public override void OnNetworkSpawn()
+    void Start()
     {
-        if (!IsServer) return;
-
         nodes = new Node[rows, columns];
 
         for (int row = 0; row < rows; row++)
@@ -41,40 +37,47 @@ public class Grid : NetworkBehaviour
                 //Instantiate the node object and get its Node component
                 GameObject nodePrefab = nodesPrefab[Random.Range(0, nodesPrefab.Count)];
                 nodePosition = new Vector3(column * gapBetweenTheNodes, 0, row * gapBetweenTheNodes);
-                GameObject nodeObject = Instantiate(nodePrefab, nodePosition, Quaternion.Euler(-90.0f, 0f, 0f), this.gameObject.transform);
-
+                GameObject nodeObject = Instantiate(nodePrefab, nodePosition, Quaternion.Euler(-90.0f,0f,0f));
+                
                 Node node = nodeObject.GetComponent<Node>();
                 node.position = new Vector3(column * gapBetweenTheNodes, 0, row * gapBetweenTheNodes);
+<<<<<<< HEAD
 
 
 
+=======
+             
+>>>>>>> parent of cb854ee... Merge branch 'multitashak' into scanning
                 //Assign the row and column of the node based on its position in the grid 
                 node.row = row;
                 node.column = column;
 
                 //Store the node in the nodes array
-                nodes[row, column] = node;
+                nodes[row, column] = node;               
                 //When you click on a node change the color of all neighbours for easier testing
                 node.OnClick.AddListener(() => ChangeNeighborColors(node));
 
                 //When you click on a node destroy it and replace it with a destroyedNode asset
+<<<<<<< HEAD
                 node.OnClick.AddListener(() => node.DestroyNode(node));
+=======
+                //node.OnClick.AddListener(() => { if (TankScript.tankChosen) node.DestroyNode(node);});
+>>>>>>> parent of cb854ee... Merge branch 'multitashak' into scanning
             }
-            //this.gameObject.GetComponent<NetworkObject>().Spawn();
         }
 
         //Assign neighbours to each node
 
-        for (int row = 0; row < rows; row++)
+        for(int row = 0; row < rows; row++)
         {
-            for (int column = 0; column < columns; column++)
+            for(int column = 0; column < columns; column++)
             {
                 Node node = nodes[row, column];
 
                 //Check neighbouring nodes in all directions
-                for (int i = -1; i <= 1; i++)
+                for(int i = -1; i <= 1; i++)
                 {
-                    for (int j = -1; j <= 1; j++)
+                    for(int j = -1; j <= 1; j++)
                     {
                         //Skip the current node
                         if (i == 0 && j == 0)
@@ -103,7 +106,7 @@ public class Grid : NetworkBehaviour
     {
         var neihbours = clickedNode.GetNeighbours();
 
-        foreach (var neighbour in neihbours)
+        foreach(var neighbour in neihbours)
         {
             neighbour.GetComponent<Renderer>().material.color = Color.red;
         }
--- Assets/Scripts/TankScript.cs
+++ Assets/Scripts/TankScript.cs
@@ -1,12 +1,12 @@
+using System.Collections;
 using System.Collections.Generic;
-using Unity.Netcode;
 using UnityEngine;
+using System;
 using UnityEngine.UI;
 
-public class TankScript : NetworkBehaviour
+public class TankScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Grid grid;
 
     public GameObject controlsPanel;
     private GameManager gameManager;
@@ -56,8 +56,15 @@ public class TankScript : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< HEAD
         if (!IsOwner) return;
         tankPlaced = !ServerScript.instance.playerTurn.Value;
+=======
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            canMove = true;
+        }
+>>>>>>> parent of cb854ee... Merge branch 'multitashak' into scanning
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit))
         {
@@ -86,7 +93,6 @@ public class TankScript : NetworkBehaviour
                     if (node.row == currentNode.row + 1 && node.column == currentNode.column && node.isWalkable)
                     {
                         neighbourNode = node;
-                        ServerScript.instance.playerTurn.Value = false;
                     }
                     break;
 
